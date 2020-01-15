1e3e2789ef69da5c3402a4f6a2e3518e3268778c
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-01-15 17:15:14 +0000

    Added service annotation

--- src/main/java/com/estafet/blockchain/demo/console/ui/service/ExchangeRateService.java
+++ src/main/java/com/estafet/blockchain/demo/console/ui/service/ExchangeRateService.java
@@ -3,11 +3,13 @@ package com.estafet.blockchain.demo.console.ui.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.estafet.blockchain.demo.console.ui.model.ExchangeRate;
 import com.estafet.microservices.scrum.lib.commons.rest.RestHelper;
 
+@Service
 public class ExchangeRateService {
 
 	@Autowired
