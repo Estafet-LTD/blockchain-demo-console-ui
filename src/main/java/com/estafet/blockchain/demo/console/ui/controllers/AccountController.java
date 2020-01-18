8956ab400991e0069ef43a9987e6b6c7f3e320e6
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-01-18 16:38:43 +0000

    new account

--- src/main/java/com/estafet/blockchain/demo/console/ui/controllers/AccountController.java
+++ src/main/java/com/estafet/blockchain/demo/console/ui/controllers/AccountController.java
@@ -38,7 +38,7 @@ public class AccountController {
 	
 	@GetMapping("/newaccount")
 	public String newAccountForm(Model model) {
-		model.addAttribute("account", new Account());		
+		model.addAttribute("newAccount", new NewAccount());		
 		model.addAttribute("rates", exchangeRateService.getExchangeRates());
 		return "newaccount";
 	}
